2f6e96989152fb43614f696b07ca70c0fc86bbcd
Roger Mayer <rmayer1984@gmail.com>
2020-04-08 13:42:39 -0500

    new youtube react repo

---
+++ src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    state= {term: '' };
+
+    onInputChange = (event) => { //event object
+        this.setState({term: event.target.value});
+    };
+
+    onFormSubmit = event =>{
+       event.preventDefault(); //prevents form from automatically being submitted
+        // todo make sure we call callback from parent form
+    };
+    render() {
+        return(
+          <div className="search-bar ui segment">
+              <form onSubmit={this.onFormSubmit} className="ui form">
+                  <div className="field">
+                      <label>Video Search</label>
+                      <input
+                          type="text" value={this.state.term}
+                          onChange={this.onInputChange}/>
+                  </div>
+              </form>
+          </div>
+        );
+    }
+}
+
+export default SearchBar;
