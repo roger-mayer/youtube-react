4d918bd30d89aa41837f8123f4f4a428a16d13c8
Roger Mayer <rmayer1984@gmail.com>
2020-04-11 13:51:43 -0500

    trying to fix max usage error

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 import env from "../.env";
 
-const myKey = process.env.REACT_APP_API_KEY;
+const KEY = process.env.REACT_APP_API_KEY;
 export default axios.create({
     baseURL: "https://www.googleapis.com/youtube/v3",
     params:{
         part: 'snippet',
         maxResults: 5,
 
-        key: myKey
+        key: KEY
     }
 
 });
