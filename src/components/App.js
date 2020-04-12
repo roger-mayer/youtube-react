b60d2b1a350daf7c9ace72032ded4f2a838931ff
Roger Mayer <rmayer1984@gmail.com>
2020-04-11 20:10:33 -0500

    added video detail component

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-    state = { videos: [] };
+    //state object (properties video and selectedVideo)
+    state = { videos: [], selectedVideo: null };
 
     onTermSubmit = async (term) =>{
         //this is called when searchbar term is submitted
@@ -15,12 +17,19 @@ class App extends React.Component {
         });
         this.setState({ videos: response.data.items});
     };
+    //callback when video is selected
+    onVideoSelect = (video) => {
+        console.log('from the app', video);
+        this.setState({selectedVideo: video});
+    };
+
     render() {
         return (
             <div className="ui container">
                 {/*runs search tem through onFormSubmit in SearchBar.jas*/}
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
             </div>
         );
     }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,10 +1,10 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({videos}) => {
+const VideoList = ({videos, onVideoSelect}) => {
   const renderedList = videos.map((video)=>{
-    // return video object from api
-    return <VideoItem video={video} />;
+    // return video array from api
+    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
   });
 
   return <div className="ui relaxed divided list">{renderedList}</div>
